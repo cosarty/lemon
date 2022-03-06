45173082e073d30dc31d638aea8422a2c32f8f57
张江航 <xncai@sifei.info>
2022-03-06 17:11:09 +0800

    feat(hook): add useListener useAway

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,3 +1,5 @@
 export * from './useState';
+export * from './userEventListener';
+export * from './usrAway';
 
 
--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import { defineComponent, ref, watch } from 'vue'
 
 export default defineComponent({
   setup() {
-    const value = ref<string>('')
+    const value = ref<string | string[]>(['a', 'b'])
 
     watch(value, () => {
       console.log(value.value)

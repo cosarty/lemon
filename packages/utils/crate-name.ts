54df3bc502c543cd877703bebdab69b74b93ef3e
蔡夏柠 <xncai@sifei.info>
2022-04-10 15:12:26 +0800

    add validate and rem

--- packages/utils/crate-name.ts
+++ packages/utils/crate-name.ts
@@ -1,3 +1,51 @@
+export type Mod = string | { [key: string]: any };
+export type Mods = Mod | Mod[];
+
+
 const namespace = 'Le'
 
-export const createName = (name: string): string => namespace + name 
+
+
+export const createName = (name: string): string => namespace + name
+
+
+function genBem(name: string, mods?: Mods): string {
+  if (!mods) {
+    return '';
+  }
+
+  if (typeof mods === 'string') {
+    return ` ${name}--${mods}`;
+  }
+
+  if (Array.isArray(mods)) {
+    return mods.reduce<string>((ret, item) => ret + genBem(name, item), '');
+  }
+
+  return Object.keys(mods).reduce(
+    (ret, key) => ret + (mods[key] ? genBem(name, key) : ''),
+    ''
+  );
+}
+
+/**
+ * bem helper
+ * b() // 'button'
+ * b('text') // 'button__text'
+ * b({ disabled }) // 'button button--disabled'
+ * b('text', { disabled }) // 'button__text button__text--disabled'
+ * b(['disabled', 'primary']) // 'button button--disabled button--primary'
+ */
+export function createBEM(name: string) {
+  name = namespace + '-' + name
+  return (el?: Mods, mods?: Mods): Mods => {
+    if (el && typeof el !== 'string') {
+      mods = el;
+      el = '';
+    }
+
+    el = el ? `${name}__${el}` : name;
+
+    return `${el}${genBem(el, mods)}`;
+  };
+}

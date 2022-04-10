0b3bc4a783fad0c14e69fc2728dbff37293d6bcc
蔡夏柠 <xncai@sifei.info>
2022-04-10 14:51:17 +0800

    add var cass

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import router from './router'
 import LemonUI from '../packages'
-import '../packages/theme/base.scss'
+
 
 createApp(App).use(LemonUI).mount('#app')
 

74214131b3b1ca904f53f96b1b7fc958941e46f3
cosart <1398675906@qq.com>
2022-07-23 15:43:01 +0800

    add

---
+++ packages/components/drawer/drawer.ts
@@ -0,0 +1,23 @@
+export const drawerProps = {
+
+}
+/* v-model	是否显示弹出层	boolean	'true','false'	false
+overlay	是否显示遮罩层	boolean	'true','false'	true
+placement	弹出位置	string	'top','left','right','bottom'	right
+round	是否显示圆角	boolean	'true','false'	true
+disable-overlay-click 是否禁用点击遮罩层后关闭	boolean	'true','false'	false
+lock-scroll
+size
+ */
+
+// open	Drawer 打开的回调	—
+
+// opened	Drawer 打开动画结束时的回调	—
+// close	Drawer 关闭的回调	—
+// closed
+
+/* 
+header
+footer
+
+*/
